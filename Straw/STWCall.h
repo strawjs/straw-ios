ddd69e393863111f8779dada9b7d94bac33b015e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-03-30 20:35:05 +0900

    Add properties

--- Straw/STWCall.h
+++ Straw/STWCall.h
@@ -1,5 +1,14 @@
 #import <Foundation/Foundation.h>
 
+
+/**
+ STWCall is the domain model which represents each Straw Call from Browser to Native.
+ */
 @interface STWCall : NSObject
+
+@property (nonatomic, retain) NSString *service;
+@property (nonatomic, retain) NSString *method;
+@property (nonatomic, retain) NSDictionary *params;
+@property (nonatomic, retain) NSNumber *callbackId;
 
 @end
