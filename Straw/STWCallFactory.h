0a74b9a4a1a4294595bd12e91c0e62f228473629
Yoshiya Hinosawa <stibium121@gmail.com>
2014-03-31 00:11:10 +0900

    Implement STWCallFactory

--- Straw/STWCallFactory.h
+++ Straw/STWCallFactory.h
@@ -1,5 +1,23 @@
 #import <Foundation/Foundation.h>
+#import "STWCall.h"
 
+/**
+ STWCallFactory is the factory class of STWCall.
+ */
 @interface STWCallFactory : NSObject
+
+/**
+ Creates STWCall object from Straw Call Request Object from Browser.
+ 
+ Call Request Object should have following fields
+
+ - service (string)
+ - method (string)
+ - params (object(dictionary))
+ - callId (number)
+
+ @param object Call Request Object from Browser.
+ */
++ (STWCall *)createFromCallRequestObject:(NSDictionary *)object;
 
 @end
--- Straw/STWCallFactory.m
+++ Straw/STWCallFactory.m
@@ -2,4 +2,16 @@
 
 @implementation STWCallFactory
 
++ (STWCall *)createFromCallRequestObject:(NSDictionary *)object
+{
+    STWCall *call = [[STWCall alloc] init];
+
+    call.service = object[@"service"];
+    call.method = object[@"method"];
+    call.params = object[@"params"];
+    call.callId = object[@"callId"];
+
+    return call;
+}
+
 @end
