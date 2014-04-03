5e6891cca955797555810b1c3788abadf95308c1
Yoshiya Hinosawa <stibium121@gmail.com>
2014-04-03 23:51:26 +0900

    Add workaround code for coverage

--- StrawTests/StrawTestObserver.m
+++ StrawTests/StrawTestObserver.m
@@ -1,11 +1,18 @@
 #import <XCTest/XCTest.h>
 
+// workaround for generating coverage data in iphonesimulator7.0
 @interface StrawTestObserver : XCTestObserver
 
 @end
 
 @implementation StrawTestObserver
 
++ (void)load
+{
+    [[NSUserDefaults standardUserDefaults] setValue:@"StrawTestObserver,XCTestLog" forKey:@"XCTestObserverClass"];
+
+    [super initialize];
+}
 
 - (void)stopObserving
 {
