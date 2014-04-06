1c3319ec4369d603f49ab1e5f3b84574a4e315c2
Yoshiya Hinosawa <stibium121@gmail.com>
2014-04-06 13:40:06 +0900

    Delete unnecessary interface declarations

--- Straw/STWWebViewDelegate.h
+++ Straw/STWWebViewDelegate.h
@@ -6,38 +6,4 @@
  */
 @interface STWWebViewDelegate : NSObject <UIWebViewDelegate>
 
-/**
-
- Called when webView failed to load.
-
- @param webView the WebView
- @param error the error
-
- */
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error;
-
-/**
-
- The hook when webView start loading.
-
- @param webView the webView
- @param request request to handle
- @param navigationType type of navigation
- */
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType;
-
-/**
- Called when webView finished loading.
-
- @param webView the webView
- */
-- (void)webViewDidFinishLoad:(UIWebView *)webView;
-
-/**
- Called when webView started loading.
-
- @param webView the webView
- */
-- (void)webViewDidStartLoad:(UIWebView *)webView;
-
 @end
