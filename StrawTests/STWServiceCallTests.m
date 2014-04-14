ef09c76bffc8aea843980d90719f8778b5464204
Yoshiya Hinosawa <stibium121@gmail.com>
2014-04-14 23:11:03 +0900

    Add tests of selector and selectorName methods

--- StrawTests/STWServiceCallTests.m
+++ StrawTests/STWServiceCallTests.m
@@ -7,9 +7,30 @@
 
 @implementation STWServiceCallTests
 
-- (void)testExample
+- (void)testConstructor
 {
     XCTAssertNotNil([[STWServiceCall alloc] init], @"STWServiceCall can construct");
 }
+
+
+- (void)testSelector
+{
+    STWServiceCall *call = [[STWServiceCall alloc] init];
+
+    call.method = @"abc";
+
+    XCTAssertEqualObjects(@"abc:withContext:", NSStringFromSelector([call selector]));
+}
+
+
+- (void)testSelectorName
+{
+    STWServiceCall *call = [[STWServiceCall alloc] init];
+
+    call.method = @"abc";
+
+    XCTAssertEqualObjects(@"abc:withContext:", [call selectorName]);
+}
+
 
 @end
