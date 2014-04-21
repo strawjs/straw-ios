737cd10dce3c75871a5268e77f77a959ffdad9b6
Yoshiya Hinosawa <stibium121@gmail.com>
2014-04-21 13:41:35 +0900

    Add the test of STWServiceCallOperation initialization

--- StrawTests/STWServiceCallOperationTests.m
+++ StrawTests/STWServiceCallOperationTests.m
@@ -7,8 +7,22 @@
 @implementation STWServiceCallOperationTests
 
 
-- (void)test
+- (void)testInitialization
 {
+    // create mocks
+    STWServiceCall *call = mock([STWServiceCall class]);
+    id<STWService> service = mockProtocol(@protocol(STWService));
+    STWNativeBridge *bridge = mock([STWNativeBridge class]);
+
+    // initialize with mocks
+    STWServiceCallOperation *operation = [[STWServiceCallOperation alloc] initWithCall:call withService:service withBridge:bridge];
+
+    XCTAssertNotNil(operation, @"STWServiceCallOperation can construct.");
+    XCTAssertEqualObjects(call, operation.serviceCall, @"serviceCall property is set properly.");
+    XCTAssertEqualObjects(service, operation.service, @"service property is set properly.");
+    XCTAssertEqualObjects(bridge, operation.bridge, @"bridge property is set properly.");
+    XCTAssertEqual(NO, operation.keepAlive, @"keepAlive propery is set to NO");
+
 }
 
 
