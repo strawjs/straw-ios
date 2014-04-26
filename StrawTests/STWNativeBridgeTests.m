d6f41436c8634eca741929ddad6aa39f5538730d
Yoshiya Hinosawa <stibium121@gmail.com>
2014-04-26 13:09:16 +0900

    Add test of bridge initialization

--- StrawTests/STWNativeBridgeTests.m
+++ StrawTests/STWNativeBridgeTests.m
@@ -7,8 +7,25 @@
 @implementation STWNativeBridgeTests
 
 
-- (void)test
+- (void)testInitWithWebViewWithViewController
 {
+    UIWebView *webView = mock([UIWebView class]);
+    UIViewController *viewController = mock([UIViewController class]);
+
+    // initialize bridge
+    STWNativeBridge *bridge = [[STWNativeBridge alloc] initWithWebView:webView withViewController:viewController];
+
+    XCTAssertNotNil(bridge, @"A bridge can init.");
+
+    XCTAssertNotNil(bridge.mainQueue, @"mainQueue property is not nil.");
+
+    XCTAssertNotNil(bridge.operationQueue, @"operationQueue property is not nil.");
+
+    XCTAssertNotNil(bridge.repository, @"repository property is not nil.");
+
+    XCTAssertEqualObjects(webView, bridge.webView, @"webView property is set properly.");
+
+    XCTAssertEqualObjects(viewController, bridge.viewController, @"viewController property is set properly.");
 }
 
 
